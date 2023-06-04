6a5392a9d324af0013d5186ab57eaa99c001f8ee
Konstantin Zlatev <kzlatev07@gmail.com>
2023-06-04 21:37:20 +0300

    logout function created

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -55,3 +55,8 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
+    
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signup')
